1af397bcd37897445244b874524b8ede83d50ea4
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-09 12:01:04 +0300

    fix eslint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 const covid19ImpactEstimator = (data) => {
@@ -5,11 +6,11 @@ const covid19ImpactEstimator = (data) => {
   return {
     data: input,
     impact() {
-      const currentlyInfected = reportedCases*10;
+      const currentlyInfected = reportedCases * 10;
       switch (periodType.toLowerCase()) {
         case 'days': {
-          const period = (timeToElapse/3).toFixed(0);
-          const infectionsByRequestedTime = (currentlyInfected * Math.pow(2, period)).toFixed(0);
+          const period = (timeToElapse / 3).toFixed(0);
+          const infectionsByRequestedTime = (currentlyInfected * 2 ** period).toFixed(0);
           const severeCasesByRequestedTime = (0.15 * infectionsByRequestedTime).toFixed(0);
           const hospitalBedsByRequestedTime = (totalHospitalBeds * 0.35).toFixed(0) - severeCasesByRequestedTime;
           const casesForICUByRequestedTime = (infectionsByRequestedTime * 0.05).toFixed();
@@ -18,9 +19,9 @@ const covid19ImpactEstimator = (data) => {
           break;
         }
         case 'weeks': {
-          const requestedTimeWeeks = timeToElapse*7;
+          const requestedTimeWeeks = timeToElapse * 7;
           const period = requestedTimeWeeks / 3;
-          const infectionsByRequestedTimeWeeks = currentlyInfected * Math.pow(2, period);
+          const infectionsByRequestedTimeWeeks = (currentlyInfected * 2 ** period).toFixed();
           const severeCasesByRequestedTimeWeeks = (0.15 * infectionsByRequestedTimeWeeks).toFixed(0);
           const hospitalBedsByRequestedTimeWeeks = (totalHospitalBeds * 0.35).toFixed(0) - severeCasesByRequestedTimeWeeks;
           const casesForICUByRequestedTimeWeeks = (infectionsByRequestedTimeWeeks * 0.05).toFixed();
@@ -31,9 +32,9 @@ const covid19ImpactEstimator = (data) => {
         case 'month': {
           const requestedTimeMonth = timeToElapse * 30;
           const period = requestedTimeMonth / 3;
-          const infectionsByRequestedTimeMonth = currentlyInfected * Math.pow(2, period);
+          const infectionsByRequestedTimeMonth = currentlyInfected * (currentlyInfected * 2 ** period).toFixed();
           const severeCasesByRequestedTimeMonth = (0.15 * infectionsByRequestedTimeMonth).toFixed(0);
-          const hospitalBedsByRequestedTimeMonth =  (totalHospitalBeds * 0.35).toFixed(0) - severeCasesByRequestedTimeMonth;
+          const hospitalBedsByRequestedTimeMonth = (totalHospitalBeds * 0.35).toFixed(0) - severeCasesByRequestedTimeMonth;
           const casesForICUByRequestedTimeMonth = (infectionsByRequestedTimeMonth * 0.05).toFixed();
           const casesForVentilatorsByRequestedTimeMonth = (infectionsByRequestedTimeMonth * 0.02).toFixed(0);
           const dollarsInFlight = 0.65 * avgDailyIncomeInUSD * period;
