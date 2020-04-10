d9153bac44fd7de2424159763c367b446f575498
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-10 15:25:45 +0300

    refactor code

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
@@ -8,61 +6,48 @@ const covid19ImpactEstimator = (data) => {
   return {
     data: input,
     impact() {
-      infectedImpact();
-      infectionsByRequestedTimeImpact();
+      const currentlyInfected = data.reportedCases * 10;
+      let period = 0;
+      switch (data.periodType.toLowerCase()) {
+        case 'days': {
+          period = data.timeToElapse / 3;
+          break;
+        }
+        case 'weeks': {
+          period = (data.timeToElapse * 7) / 3;
+          break;
+        }
+        case 'months': {
+          period = (data.timeToElapse * 30) / 3;
+          break;
+        }
+        default:
+          break;
+      }
+      const infectionsByRequestedTime = (currentlyInfected * 2 ** period).toFixed(0);
     },
     severeImpact() {
-      infectedSevereImpact();
-      infectionsByRequestedTimeSevereImpact();
+      const currentlyInfected = data.reportedCases * 10;
+      let period = 0;
+      switch (data.periodType.toLowerCase()) {
+        case 'days': {
+          period = data.timeToElapse / 3;
+          break;
+        }
+        case 'weeks': {
+          period = (data.timeToElapse * 7) / 3;
+          break;
+        }
+        case 'months': {
+          period = (data.timeToElapse * 30) / 3;
+          break;
+        }
+        default:
+          break;
+      }
+      const infectionsByRequestedTime = (currentlyInfected * 2 ** period).toFixed(0);
     }
   };
 };
-
-function infectedImpact() {
-  return data.reportedCases * 10;
-}
-function infectedSevereImpact() {
-  return data.reportedCases * 50;
-}
-function infectionsByRequestedTimeImpact() {
-  let period = 0;
-  switch (data.periodType.toLowerCase()) {
-    case 'days': {
-      period = data.timeToElapse / 3;
-      break;
-    }
-    case 'weeks': {
-      period = (data.timeToElapse * 7) / 3;
-      break;
-    }
-    case 'months': {
-      period = (data.timeToElapse * 30) / 3;
-      break;
-    }
-    default:
-      break;
-  }
-  return parseInt((infectedImpact() * 2 ** period).toFixed(0), 10);
-}
-function infectionsByRequestedTimeSevereImpact() {
-  let period = 0;
-  switch (data.periodType.toLowerCase()) {
-    case 'days': {
-      period = data.timeToElapse / 3;
-      break;
-    }
-    case 'weeks': {
-      period = (data.timeToElapse * 7) / 3;
-      break;
-    }
-    case 'months': {
-      period = (data.timeToElapse * 30) / 3;
-      break;
-    }
-    default:
-      break;
-  }
-  return parseInt((infectedSevereImpact() * 2 ** period).toFixed(0), 10);
-}
 
 export default covid19ImpactEstimator;
