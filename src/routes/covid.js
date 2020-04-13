76bcc9d2aeec6bdffdf89cd2a37594486da7e9f8
ShadrackAdwera <adweshshaddie@gmail.com>
2020-04-14 02:56:46 +0300

    post using json endpoint

--- src/routes/covid.js
+++ src/routes/covid.js
@@ -26,8 +26,22 @@ router.post('/', (req, res, next) => {
 });
 
 router.post('/json', (req, res, next) => {
-  res.status(201).json({
+  const info = {
+    region: {
+      name: req.body.region.name,
+      avgAge: req.body.region.avgAge,
+      avgDailyIncomeInUSD: req.body.region.avgDailyIncomeInUSD,
+      avgDailyIncomePopulation: req.body.region.avgDailyIncomePopulation
+    },
+    periodType: req.body.periodType,
+    timeToElapse: req.body.timeToElapse,
+    reportedCases: req.body.reportedCases,
+    population: req.body.population,
+    totalHospitalBeds: req.body.totalHospitalBeds
 
+  };
+  res.status(201).json({
+    information: estimator(info)
   });
 });
 
